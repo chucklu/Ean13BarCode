2e5352540c4e8e43dacd23e72f6f034adfe4687f
Chuck Lu <chuck.lu@qq.com>
2017-09-05 17:15:42 +0800

    refactor code with resharper

--- Ean13Barcode2005/frmEan13.cs
+++ Ean13Barcode2005/frmEan13.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Drawing;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Drawing.Printing;
 using System.Windows.Forms;
-using System.Data;
 
 namespace Ean13Barcode2005
 {
@@ -13,15 +10,15 @@ namespace Ean13Barcode2005
 	/// </summary>
 	partial class frmEan13 : Form
 	{
-		private Ean13 ean13 = null;
+		private Ean13 ean13;
 
 		public frmEan13( )
 		{
 			InitializeComponent( );
 			cboScale.SelectedIndex = 2;
-		}
+        }
 
-		private void CreateEan13( )
+	    private void CreateEan13( )
 		{
 			ean13 = new Ean13( );
 			ean13.CountryCode = txtCountryCode.Text;
@@ -33,30 +30,30 @@ namespace Ean13Barcode2005
 
 		private void butDraw_Click(object sender, EventArgs e)
 		{
-			System.Drawing.Graphics g = this.picBarcode.CreateGraphics( );
+			Graphics g = picBarcode.CreateGraphics( );
 
-			g.FillRectangle( new System.Drawing.SolidBrush( System.Drawing.SystemColors.Control ),
+			g.FillRectangle( new SolidBrush( SystemColors.Control ),
 				new Rectangle( 0, 0, picBarcode.Width, picBarcode.Height ) );
 
 			CreateEan13( );
 			ean13.Scale = (float)Convert.ToDecimal( cboScale.Items [cboScale.SelectedIndex] );
-			ean13.DrawEan13Barcode( g, new System.Drawing.Point( 0, 0 ) );
+			ean13.DrawEan13Barcode( g, new Point( 0, 0 ) );
 			txtChecksumDigit.Text = ean13.ChecksumDigit;
 			g.Dispose( );
 		}
 
 		private void butPrint_Click(object sender, EventArgs e)
 		{
-			System.Drawing.Printing.PrintDocument pd = new System.Drawing.Printing.PrintDocument( );
-			pd.PrintPage += new System.Drawing.Printing.PrintPageEventHandler( this.pd_PrintPage );
+			PrintDocument pd = new PrintDocument( );
+			pd.PrintPage += pd_PrintPage;
 			pd.Print( );
 		}
 
-		private void pd_PrintPage( object sender, System.Drawing.Printing.PrintPageEventArgs ev )
+		private void pd_PrintPage( object sender, PrintPageEventArgs ev )
 		{
 			CreateEan13( );
 			ean13.Scale = ( float )Convert.ToDecimal( cboScale.Items [cboScale.SelectedIndex] );
-			ean13.DrawEan13Barcode( ev.Graphics, new System.Drawing.Point( 0, 0 ) );
+			ean13.DrawEan13Barcode( ev.Graphics, new Point( 0, 0 ) );
 			txtChecksumDigit.Text = ean13.ChecksumDigit;
 
 			// Add Code here to print other information.
@@ -64,14 +61,14 @@ namespace Ean13Barcode2005
 		}
 
 		private void butCreateBitmap_Click(object sender, EventArgs e)
-		{
-			CreateEan13( );
-			ean13.Scale = ( float )Convert.ToDecimal( cboScale.Items [cboScale.SelectedIndex] );
+        {
+            CreateEan13();
+            ean13.Scale = (float)Convert.ToDecimal(cboScale.Items[cboScale.SelectedIndex]);
 
-			System.Drawing.Bitmap bmp = ean13.CreateBitmap( );
-			this.picBarcode.Image = bmp;
-		}
+            Bitmap bmp = ean13.CreateBitmap();
+            picBarcode.Image = bmp;
+        }
 
-	}
+    }
 }
 
