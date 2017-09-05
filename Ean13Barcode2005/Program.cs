652b0ee7fe306ca4372e5dd632b684f93e4a8b02
Chuck Lu <chuck.lu@qq.com>
2017-09-05 17:33:26 +0800

    catch the exception and show it with message box

--- Ean13Barcode2005/Program.cs
+++ Ean13Barcode2005/Program.cs
@@ -1,7 +1,6 @@
 ﻿#region Using directives
 
 using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 
 #endregion
@@ -16,8 +15,17 @@ namespace Ean13Barcode2005
 		[STAThread]
 		static void Main( )
 		{
-			Application.EnableVisualStyles( );
-			Application.Run( new frmEan13( ) );
+		    Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+		    Application.ThreadException += Application_ThreadException;
+
+		    Application.EnableVisualStyles();
+		    Application.SetCompatibleTextRenderingDefault(false);
+		    Application.Run(new frmEan13());
 		}
-	}
+
+	    static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+	    {
+	        MessageBox.Show(e.Exception.Message, @"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+	    }
+    }
 }
